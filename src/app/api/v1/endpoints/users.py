7176f16fda62ca83affddc4d03f0b84ca72f46da
Nikhil M Jain <nikhilporwal94@gmail.com>
2020-04-04 17:26:53 +0530

    Set location header in response object of create user function

--- src/app/api/v1/endpoints/users.py
+++ src/app/api/v1/endpoints/users.py
@@ -1,29 +1,31 @@
 import secrets
-from typing import List
 
 from fastapi import Depends, APIRouter
+from starlette.responses import Response
 
 from src.app.api.v1.auth import get_auth_user
+from src.app.api.v1.config import API_PREFIX
 from src.app.api.v1.models import UserResponse, User, NewUser
-from src.app.database import users, database
+from src.app.database import database, users
 
 router = APIRouter()
 
 
-@router.get('/v1/users/', response_model=List[UserResponse])
-async def get_all_users(current_user: User = Depends(get_auth_user)):
-    query = users.select()
-    all_users = await database.fetch_all(query)
-    return all_users
+@router.get('/v1/users/{user_id}', response_model=UserResponse)
+async def get_users(user_id: int = None, current_user: User = Depends(get_auth_user)):
+    query = users.select().where(users.c.user_id == user_id)
+    return await database.fetch_one(query)
 
 
 @router.post('/users/')
-async def add_new_user(payload: NewUser):
+async def add_new_user(payload: NewUser, response: Response):
     token = secrets.token_hex(20)
-    query = users.insert().values(email=payload.email, token=token)
-    await database.execute(query)
+    query = users.insert().values(email=payload.email, token=token, first_name=payload.first_name,
+                                  last_name=payload.last_name)
+    user_id = await database.execute(query)
     response_object = {
         'email': payload.email,
         'token': token
     }
+    response.headers['Location'] = '{prefix}/users/{user_id}'.format(prefix=API_PREFIX, user_id=user_id)
     return response_object
