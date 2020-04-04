edd6e682462ed5c5c96126c3bc1e113ba38bc77d
Nikhil M Jain <nikhilporwal94@gmail.com>
2020-04-04 17:25:14 +0530

    Use .env file to read constants and credentials

--- src/app/api/v1/config.py
+++ src/app/api/v1/config.py
@@ -1,3 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SERVER_NAME = os.getenv('SERVER_NAME')
+BASE_URL = os.getenv('BASE_URL')
+
+API_PREFIX = '{}{}'.format(SERVER_NAME, BASE_URL)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
