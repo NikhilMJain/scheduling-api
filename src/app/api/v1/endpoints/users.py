10bc5862d45651b91a7dea0d042d335cb8d874a2
Nikhil M Jain <nikhilporwal94@gmail.com>
2020-04-05 10:55:06 +0530

    Add tags to all routes for categorization in docs page

--- src/app/api/v1/endpoints/users.py
+++ src/app/api/v1/endpoints/users.py
@@ -11,13 +11,13 @@ from src.app.database import database, users
 router = APIRouter()
 
 
-@router.get('/v1/users/{user_id}', response_model=UserResponse)
+@router.get('/v1/users/{user_id}', response_model=UserResponse, tags='users')
 async def get_users(user_id: int = None, current_user: User = Depends(get_auth_user)):
     query = users.select().where(users.c.user_id == user_id)
     return await database.fetch_one(query)
 
 
-@router.post('/v1/users/')
+@router.post('/v1/users/', tags='users')
 async def add_new_user(payload: NewUser, response: Response):
     token = secrets.token_hex(20)
     query = users.insert().values(email=payload.email, token=token, first_name=payload.first_name,
