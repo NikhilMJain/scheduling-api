f5e7c49d6ff8261f6ee6adf1afe1637863df5526
Nikhil M Jain <nikhilporwal94@gmail.com>
2020-04-06 18:10:47 +0530

    Use calendar ID from environment file

--- src/app/api/v1/config.py
+++ src/app/api/v1/config.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 SERVER_NAME = os.getenv('SERVER_NAME')
 BASE_URL = os.getenv('BASE_URL')
-
 API_PREFIX = '{}{}'.format(SERVER_NAME, BASE_URL)
-
 DATABASE_URL = os.getenv('DATABASE_URL')
-
-TEST_DB = os.getenv('TEST_DB')
+CALENDAR_ID = os.getenv('CALENDAR_ID')
